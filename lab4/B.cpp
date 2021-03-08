1be5ba6ba9558bda1447e561f6f8f190f12466bf
podobry-m <podobrii.ma@phystech.edu>
2021-03-08 22:15:00 +0300

    Update B.cpp

--- lab4/B.cpp
+++ lab4/B.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int pop(vector<int> &v){
     int b = v[v.size()-1];
-    
+
     v.pop_back();
     return b;
 }
@@ -17,12 +17,23 @@ int main()
         }
         else if(s=="-")
         {
-            v.push_back(pop(v)-pop(v));
+            int a=pop(v);
+            int b=pop(v);
+            v.push_back(b-a);
+        }
+        else if(s=="*")
+            v.push_back(pop(v)*pop(v));
+        else if(s=="/")
+        {
+            int a=pop(v);
+            int b=pop(v);
+            v.push_back(b/a);
         }
         else
             v.push_back(stoi(s));
     }
-    
+    cout<<v[0];
+
     return 0;
 }
 
